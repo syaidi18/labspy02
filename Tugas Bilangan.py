ef6f72b6f3890b150188475b3eafebf27f27be64
syaidi18 <56240679+syaidi18@users.noreply.github.com>
2019-10-26 16:12:42 +0700

    Add files via upload

---
+++ Tugas Bilangan.py
@@ -0,0 +1,11 @@
+a = int(input("Masukkan bilangan A: "))
+b = int(input("Masukkan bilangan B: "))
+c = int(input("Masukkan bilangan C: "))
+
+if a > b and a > c:
+    print ( " Bilangan terbesarnya adalah: " , a)
+elif b > a and b > c:
+    print ( " Bilangan terbesarnya adalah: " , b)
+elif c > a and c > b:
+    print ( " Bilangan terbesarnya adalah: " , c)
+    
